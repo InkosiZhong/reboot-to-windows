414b20a63c17f72b6583d5045a8e5f77e39cee02
inkosizhong <inkosizhong@gmail.com>
2023-11-24 08:51:06 +0800

    update

--- r2w.sh
+++ r2w.sh
@@ -10,12 +10,26 @@ do
     name=$(echo "$line" | grep -o "'[^']*'" | head -n 1 | cut -c 2- | rev | cut -c 2- | rev)
     echo "$i: $name"
 done
-echo "\nSelect the index of the target system:"
-read target_index
-line=$(echo "$menu" | sed -n "${target_index}p")
-name=$(echo "$line" | grep -o "'[^']*'" | head -n 1 | cut -c 2- | rev | cut -c 2- | rev)
-echo "Selected system: ${name}"
-sudo grub-reboot $((target_index - 1))
-sudo reboot
-
 rm /tmp/grub_config.tmp
+case $# in
+0) 
+    echo "\nSelect the index of the target system:"
+    read target_index
+    ;;
+1) 
+    target_index=$1
+    ;;
+*)
+    echo "usage: r2w [sys]"
+    exit
+esac
+if [ $target_index -gt 0 ] && [ $target_index -le $num ] 2>/dev/null ;
+then 
+    line=$(echo "$menu" | sed -n "${target_index}p")
+    name=$(echo "$line" | grep -o "'[^']*'" | head -n 1 | cut -c 2- | rev | cut -c 2- | rev)
+    echo "Selected system: ${name}"
+    sudo grub-reboot $((target_index - 1))
+    sudo reboot
+else 
+    echo "$target_index is invalid."
+fi 
